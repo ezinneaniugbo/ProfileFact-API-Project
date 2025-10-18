b703aa13173342c50d03410abd027574eaf65f90
Ezinne <ezinneaniugbo@Ezinnes-MacBook-Pro.local>
2025-10-19 00:33:46 +0100

    Fixed Flask import issue and updated code

--- app.py
+++ app.py
@@ -36,5 +36,4 @@ def get_profile():
     return jsonify(profile), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
